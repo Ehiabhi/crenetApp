8161f5c4509713ea1a3319ddfc92fe898cd805a9
Ehiabhi <enimakpokpo@yahoo.com>
2020-08-01 00:20:44 +0100

    add details to gitignore files

--- config/database.js
+++ config/database.js
@@ -1,4 +1,6 @@
+require("dotenv").config();
+
 module.exports = {
-    database: "mongodb+srv://admin-ehis:f1i9r9s8t9@cluster0-5p0bt.mongodb.net/cmsCart"
+    database: "mongodb+srv://admin-ehis:" + process.env.PASSWORD + "@cluster0-5p0bt.mongodb.net/cmsCart"
 
 }
